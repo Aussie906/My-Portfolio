a412e8450a0f47e805ef96040b52893812d13bd1
Aussie906 <austin.laird98@gmail.com>
2025-11-06 09:18:36 -0600

    Will fix later
    not firing

--- contactMe/scripts/script.js
+++ contactMe/scripts/script.js
@@ -23,11 +23,46 @@
                             .map(cb => cb.value)
                             .join(", ") || "None";
 
+
+      // Create an array of key-value pairs
+      const formdata = [
+        ["Name", name],
+        ["Email", email],
+        ["Password", password],
+        ["Phone", phone],
+        ["Website", website],
+        ["Age", age],
+        ["Date of Birth", dob],
+        ["Preferred Time", time],
+        ["Color", color],
+        ["Interest Level", range],
+        ["Contact Method", contactMethod],
+        ["Services Interested In", services]
+        ["Topic", topic],
+        ["Message", message]
+      ];
+      
+      // Showing results section
+      const resultcontainer =
+      document.getElementById("resultContainer");
+      const resultBody = document.querySelector("#resultTable tbody");
+      resultBody.InnerHTML =""; //Clear previous entries each time
+      resultcontainer.style.display = "block"; //Displaying the table
+
+      // Populate the table
+      formdata.forEach(([key, value]) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td>${key}</td><td>${value}`; // 
+        resultBody.appendChild(row); // Stacks rows until done
+      }  
+    );
+
       // Display everything in an alert
-      alert(
-        `--- CONTACT FORM DATA ---\n\n` +
-        `Name: ${name}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}\nWebsite: ${website}\nAge: ${age}\nDate of Birth: ${dob}\nPreferred Time: ${time}\nFavorite Color: ${color}\nInterest Level: ${range}\nContact Method: ${contactMethod}\nServices Interested In: ${services}\nTopic: ${topic}\nMessage: ${message}`
-      );
+      // Old code from previous assignment
+      // alert(
+      //   `--- CONTACT FORM DATA ---\n\n` +
+      //   `Name: ${name}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}\nWebsite: ${website}\nAge: ${age}\nDate of Birth: ${dob}\nPreferred Time: ${time}\nFavorite Color: ${color}\nInterest Level: ${range}\nContact Method: ${contactMethod}\nServices Interested In: ${services}\nTopic: ${topic}\nMessage: ${message}`
+      // );
 
       // Reset the form
       this.reset();
